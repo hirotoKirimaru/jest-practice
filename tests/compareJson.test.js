83fc0cde839b6d5623bb4d62be798f1c51f8645b
kirimaru <nainaistar@gmail.com>
2020-09-19 01:38:31 +0000

    [Add] listや今後無視したい項目が必要

--- tests/compareJson.test.js
+++ tests/compareJson.test.js
@@ -1,9 +1,17 @@
-describe('OKStatus', () => {
-    test('完全比較', () => {
-        const response = require("./resources/_200statusBody.json");
-        const expected = 
-        { "hello":"Hello, Tom!"
+describe('StatusCode 200', () => {
+    const response = require("./resources/200/01.json");
 
+    test('完全比較', () => {
+        const expected =
+        {
+            "hello": "Hello, Tom!",
+            "animals": [
+                "gorilla",
+                "chin-pan",
+                "human"
+            ],
+            "generate_date": "2020091910011234",
+            "update_date": "2020092010023456"
         };
         expect(response).toEqual(expected);
     });
