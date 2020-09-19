00539d05062b178ae6e9cd56d8abb7817684d381
kirimaru <nainaistar@gmail.com>
2020-09-19 02:15:30 +0000

    [Add] 順番無視

--- tests/compareJson.test.js
+++ tests/compareJson.test.js
@@ -16,7 +16,7 @@ describe('StatusCode 200', () => {
         expect(response).toEqual(expected);
     });
 
-    
+
     test('時間を完全無視', () => {
         const expected =
         {
@@ -32,5 +32,38 @@ describe('StatusCode 200', () => {
         expect(response).toEqual(expected);
     });
 
+    test('時間を年月日まで無視', () => {
+        const expected =
+        {
+            "hello": "Hello, Tom!",
+            "animals": [
+                "gorilla",
+                "chin-pan",
+                "human"
+            ],
+            "generate_date": expect.stringMatching("2020091910.*"),
+            "update_date": expect.stringMatching("2020092010.*")
+        };
+        // 下記ならちゃんと失敗してくれる
+        // "update_date": expect.stringMatching("2020091910.*")
+
+        expect(response).toEqual(expected);
+    });
+
+    test('順番は無視', () => {
+        const expected =
+        {
+            "hello": "Hello, Tom!",
+            "animals": expect.arrayContaining([
+                "chin-pan",
+                "gorilla",
+                "human"
+            ]),
+            "generate_date": expect.anything(),
+            "update_date": expect.anything()
+        };
+
+        expect(response).toEqual(expected);
+    });
 
 })
